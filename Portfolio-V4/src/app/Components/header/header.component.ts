fdc3513f6196b91d29ea88ac66c89c60f78c7d6c
Mohamed Atef <go3t.10@gmail.com>
2023-05-06 01:24:18 +0300

    Updated Changes V-06

--- Portfolio-V4/src/app/Components/header/header.component.ts
+++ Portfolio-V4/src/app/Components/header/header.component.ts
@@ -1,27 +1,26 @@
-import { Component, HostListener , Input, OnInit } from '@angular/core'
+import { Component, HostListener, Input, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
   @Input() isLoading!: boolean;
   showList = false;
 
-
   constructor(private router: Router) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   downloadResume() {
     const link = document.createElement('a');
-    const resume = 'https://drive.google.com/file/d/1xeQ_UY1q4Z5kkn_8xhp5BuAVg9j46cKb/view?usp=sharing'
+    const resume =
+      'https://drive.google.com/file/d/1TBqpUijyZk6mDpbAiss4YuloAPt6PDq5/view?usp=sharing';
     link.setAttribute('href', resume);
     link.setAttribute('download', 'resume.pdf');
-    link.setAttribute('target', '_blank')
+    link.setAttribute('target', '_blank');
     link.style.display = 'none';
     document.body.appendChild(link);
     link.click();
